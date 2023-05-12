6b5ffe1848fc7dc883db2a7bd1af2df4ed12789a
marnixah <marnix@roboco.dev>
2023-05-12 14:46:38 +0200

    autoload .env files :3

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SmallTalkExceptionFilter } from './smalltalk/smalltalk-exception.filter';
-
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService, SmallTalkExceptionFilter],
 })
